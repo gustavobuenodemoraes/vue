ac8a6d950a80eefdb63ad4dd3ecf5106c87a9dc1
Gustavo Bueno de Moraes <gustavo_beuno@hotmail.com>
2022-06-07 22:29:58 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="columns is-one-quarter"></div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style>
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,13 @@
+<template>
+<header>
+    <h1>
+        <img src="../assets/logo.png" alt="">
+    </h1>
+</header>
+</template>
+
+<script lang="ts">
+</script>
+
+<style scoped>
+</style>
